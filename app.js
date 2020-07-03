d5a51a1e7b0ce14d19eb48e79f1c3240e8f2a36c
sankt <bsankt16@gmail.com>
2020-07-03 20:44:39 +0530

    Express Generator REST API fix

--- app.js
+++ app.js
@@ -24,9 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('./dishes', dishRouter)
-app.use('./promotions', promoRouter)
-app.use('./leaders', leadersRouter)
+app.use('/dishes', dishRouter)
+app.use('/promotions', promoRouter)
+app.use('/leaders', leadersRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
